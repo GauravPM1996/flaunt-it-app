a9f61a8fe04fbdf3075c9a4390f8eadc2976d822
GauravPM1996 <iitrgauravsingh23@gmail.com>
2025-07-04 11:06:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Import your App component
+import App from '.src/app'; // Import your App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
